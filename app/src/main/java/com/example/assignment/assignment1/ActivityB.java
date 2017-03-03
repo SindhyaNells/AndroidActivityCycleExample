da41e87e884d6c2f70064ff20610e510064534f9
sindhya <sindhyakumari90@gmail.com>
2017-03-03 02:27:48 -0800

    Added Activity B

--- app/src/main/java/com/example/assignment/assignment1/ActivityB.java
+++ app/src/main/java/com/example/assignment/assignment1/ActivityB.java
@@ -2,12 +2,25 @@ package com.example.assignment.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityB extends AppCompatActivity {
+
+    private Button btnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+
+        btnFinish=(Button)findViewById(R.id.btn_finish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityB.this.finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/assignment/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment/assignment1/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.assignment.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnActivity;
     private Button btnDialog;
@@ -16,7 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnActivity=(Button)findViewById(R.id.btn_activity);
+        btnActivity.setOnClickListener(this);
         btnDialog=(Button)findViewById(R.id.btn_dialog);
+        btnDialog.setOnClickListener(this);
+        btnCloseApp=(Button)findViewById(R.id.btn_close_app);
+        btnCloseApp.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+        switch (id){
+            case R.id.btn_activity:
+                Intent activity_intent=new Intent(MainActivity.this,ActivityB.class);
+                startActivity(activity_intent);
+                break;
+            case R.id.btn_dialog:
+                break;
+            case R.id.btn_close_app:
+                MainActivity.this.finish();
+                break;
+        }
     }
 }
