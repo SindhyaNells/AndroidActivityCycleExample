4fb4b114cb13be6945d9694c34aeb4dbc9f28fd4
sindhya <sindhyakumari90@gmail.com>
2017-03-03 14:59:34 -0800

    added dialog activity

--- app/src/main/java/com/example/assignment/assignment1/ActivityB.java
+++ app/src/main/java/com/example/assignment/assignment1/ActivityB.java
@@ -1,5 +1,6 @@
 package com.example.assignment.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.Button;
 public class ActivityB extends AppCompatActivity {
 
     private Button btnFinish;
-
+    private int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +19,23 @@ public class ActivityB extends AppCompatActivity {
         btnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent main_intent=new Intent();
+                main_intent.putExtra("count",count);
+                setResult(RESULT_OK,main_intent);
                 ActivityB.this.finish();
             }
         });
 
+        count=getIntent().getExtras().getInt("count");
+
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        count++;
+    }
+
+
+
 }
--- app/src/main/java/com/example/assignment/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment/assignment1/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.assignment.assignment1;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnActivity;
     private Button btnDialog;
     private Button btnCloseApp;
+    private TextView textCount;
+    private int count=0;
+    private static final int ACTIVITYB_RESULT_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCount=(TextView)findViewById(R.id.thread_count);
 
         btnActivity=(Button)findViewById(R.id.btn_activity);
         btnActivity.setOnClickListener(this);
@@ -27,18 +35,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onResume(){
+        super.onResume();
+        count++;
+        Log.d("count",String.valueOf(count));
+        textCount.setText(String.valueOf(count));
+    }
+
+    @Override
     public void onClick(View view) {
         int id=view.getId();
         switch (id){
             case R.id.btn_activity:
                 Intent activity_intent=new Intent(MainActivity.this,ActivityB.class);
-                startActivity(activity_intent);
+                activity_intent.putExtra("count",count);
+                startActivityForResult(activity_intent,ACTIVITYB_RESULT_CODE);
                 break;
             case R.id.btn_dialog:
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_text).setTitle(R.string.dialog_label);
+                AlertDialog dialog= builder.create();
+                dialog.show();
+
                 break;
             case R.id.btn_close_app:
                 MainActivity.this.finish();
                 break;
         }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==ACTIVITYB_RESULT_CODE){
+            if(resultCode==RESULT_OK){
+                int res_count= data.getExtras().getInt("count");
+                count=res_count;
+            }
+        }
+    }
+
 }
